3325e1fba180350a9c59ccbcfea9c1ab2f369e53
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-09-27 14:55:56 +0300

    create models, add app to settings INSTALLED_APPS, create agency/urls.py and include it to main urls.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+	years_of_experience = models.PositiveIntegerField()
+	
+	class Meta:
+		verbose_name = 'redactor'
+		verbose_name_plural = 'redactors'
+
+
+class Topic(models.Model):
+	name = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.name
+	
+	class Meta:
+		verbose_name = 'topic'
+		verbose_name_plural = 'topics'
+		ordering = ['name']
+		
+		
+class Newspaper(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	published_date = models.DateTimeField(auto_now_add=True)
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	publishers = models.ManyToManyField(Redactor)
+	
+	def __str__(self):
+		return self.title
+	
+	class Meta:
+		verbose_name = 'newspaper'
+		verbose_name_plural = 'newspapers'
+		ordering = ['name']
--- agency/urls.py
+++ agency/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("admin/", admin.site.urls),
+    path("", include("agency.urls", namespace="agency")),
 ]
-
-app_name = "agency"
