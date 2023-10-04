1edfa616e453aaa25a6e177d8bdb5b977215feb4
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-04 14:36:21 +0300

    1. use default_related_name in Redactor model and remove groups and user_permissions field
    2. relocate class Meta to be right under the fields

--- agency/models.py
+++ agency/models.py
@@ -11,32 +11,19 @@ class Redactor(AbstractUser):
     class Meta:
         verbose_name = 'redactor'
         verbose_name_plural = 'redactors'
-
-    # Specify related names to resolve clashes
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name=('groups',),
-        blank=True,
-        related_name='redactors'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name=('user permissions',),
-        blank=True,
-        related_name='redactors'
-    )
+        default_related_name = 'redactors'
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name = 'topic'
         verbose_name_plural = 'topics'
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
     def image_name(self):
         return self.name.lower().replace(' ', '_') + '.svg'
@@ -50,13 +37,13 @@ class Newspaper(models.Model):
     publishers = models.ManyToManyField(Redactor, related_name='newspapers')
     image = models.ImageField(upload_to='newspaper_images', null=True)
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name = 'newspaper'
         verbose_name_plural = 'newspapers'
         ordering = ['-published_date']
+
+    def __str__(self):
+        return self.title
 
     def get_short_content(self):
         if len(self.content) > 75:
