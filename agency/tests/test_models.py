39d0dcf0edfb27f4d3ca2902b9141bbccce8bbf8
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-04 16:05:03 +0300

    create tests for models str methods

---
+++ agency/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from agency.models import Redactor, Topic, Newspaper
+
+
+class ModelTest(TestCase):
+	def setUp(self) -> None:
+		self.redactor = get_user_model().objects.create_user(
+			username='test',
+			password='123',
+			first_name='Test',
+			last_name='Test',
+		)
+		self.topic = Topic.objects.create(name='Test')
+		self.newspaper = Newspaper.objects.create(
+			title='Test',
+			content='Test',
+			topic=self.topic,
+		)
+		self.newspaper.publishers.add(self.redactor)
+
+	def test_redactor_str(self):
+		self.assertEqual(str(self.redactor), self.redactor.username)
+
+	def test_topic_str(self):
+		self.assertEqual(str(self.topic), self.topic.name)
+
+	def test_newspaper_str(self):
+		self.assertEqual(str(self.newspaper), self.newspaper.title)
